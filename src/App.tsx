6fdfa304b50e915a5a89210e73c98a4d118cfe6c
castellano42 <andrewjosephc42@gmail.com>
2022-12-09 11:11:11 -0800

    3 files modified, 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="h-screen w-screen">
       <div className="grid grid-rows-3 grid-flow-col gap-4">
-        <div className="row-span-3 bg-gray-700 h-screen">01</div>
-        <div className="col-span-2">02</div>
+        <aside className="row-span-3 bg-gray-700 h-screen">01</aside>
+        <header className="col-span-2">02</header>
         <div className="row-span-2 col-span-2">03</div>
       </div>
     </div>
