b70458e8339d43fa09c1cbb083fbd3af6debcb38
castellano42 <andrewjosephc42@gmail.com>
2022-12-13 13:26:16 -0800

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,17 @@ import BookGrid from './components/BookGrid';
 function App() {
   return (
     <div
-      className="grid 
-    grid grid-rows-[60px_1fr_1fr_120px]
-    grid-rows-3 grid-flow-col h-screen w-screen"
+      className="grid grid-rows-[50px_1fr_1fr_120px]
+     grid-flow-col h-screen w-screen"
     >
       <aside className="row-span-3 bg-gray-900 ">
         <SideNav />
       </aside>
-      <div className="row-span-1 col-span-3">04</div>
-      <header className="col-span-2 bg-gray-700">
+      <div className="col-span-4 bg-gray-800">Bottom Bar</div>
+      <div className="row-span-1 col-span-3 bg-gray-700">
         <TopNav />
-      </header>
-      <div className="row-span-1 col-span-2 bg-gray-700"><BookGrid/></div>
+      </div>
+      <main className="col-span-3 col-span-2">Bo</main>
     </div>
   );
 }
--- src/components/BookGrid.tsx
+++ src/components/BookGrid.tsx
@@ -1,6 +1,6 @@
 function BookGrid(){
   return(
-    <div className="grid grid-rows-4 grid-flow-col gap-4 p-5">
+    <div className="flex grid grid-rows-4 grid-flow-col gap-16 p-5">
       <BookCard image="https://dsxuu8etcj8kw.cloudfront.net/e/u/eu5c/eu5c-square-400.jpg" title="The Night Shift" author="Alex Finlay"/>
       <BookCard image="https://dsxuu8etcj8kw.cloudfront.net/e/u/eu5c/eu5c-square-400.jpg" title="The Night Shift" author="Alex Finlay"/>
       <BookCard image="https://dsxuu8etcj8kw.cloudfront.net/e/u/eu5c/eu5c-square-400.jpg" title="The Night Shift" author="Alex Finlay"/>
@@ -25,7 +25,7 @@ function BookCard({image, title, author}){
   return(
     <div className="h-40 w-40">
       <img src={image} alt="No Image"/>
-      <div>
+      <div className="p-2">
         {title}
         by {author}
         </div>
