fd9ea135d918027e7e52ee1cd1f239b30e6bdfd1
castellano42 <andrewjosephc42@gmail.com>
2023-01-05 12:18:05 -0800

    1 file modified, 1 added, 1 deleted

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root/>,
+    path: '/',
+    element: <Root />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ErrorPage />
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
 
-export default function Root(){
-  return(
-    <App/>
-  )
+export default function Root() {
+  return <App />;
 }
